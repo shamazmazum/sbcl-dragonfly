461f48725f4e7288ba70fe87d3ef09deed12e8e5
Vasily Postnicov <shamaz.mazum@gmail.com>
2014-04-16 19:59:07 +0400

    sb-sprof test for freebsd was fixed, so bring it back

--- contrib/sb-sprof/test.lisp
+++ contrib/sb-sprof/test.lisp
@@ -38,7 +38,7 @@
 
 #-(or win32 darwin)                    ;not yet
 (test)
-#-(or win32 darwin freebsd)            ;not yet
+#-(or win32 darwin)            ;not yet
 (consing-test)
 
 ;; For debugging purposes, print output for visual inspection to see if
